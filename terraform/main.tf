8428648a8c6cf410ad3aadf28e4389c18482b402
p-kali <pravin.k.kalimuthu@gmail.com>
2026-04-09 18:02:46 -0400

    Defaulting H2 database

--- terraform/main.tf
+++ terraform/main.tf
@@ -53,6 +53,42 @@ resource "aws_security_group" "app_sg" {
   }
 }
 
+#Role
+resource "aws_iam_role" "ec2_role" {
+  name = "devops-ec2-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+#Policy ECR read
+resource "aws_iam_role_policy_attachment" "ecr_read" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+#Policy SSM read
+resource "aws_iam_role_policy_attachment" "ssm_read" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMReadOnlyAccess"
+}
+
+#Create profile/role
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "devops-ec2-profile"
+  role = aws_iam_role.ec2_role.name
+}
+
 resource "aws_instance" "devops_demo_ec2" {
   #ami = "ami-0ec10929233384c7f"
   #instance_type = "t3.micro"
@@ -61,6 +97,7 @@ resource "aws_instance" "devops_demo_ec2" {
   instance_type = var.instance_type
   key_name = var.key_name
   vpc_security_group_ids = [aws_security_group.app_sg.id]
+  iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
   subnet_id = aws_subnet.public_subnet_1.id
   tags = {
     Name = "devops-demo-ec2"
@@ -81,6 +118,12 @@ resource "aws_instance" "devops_demo_ec2" {
               systemctl start docker
               systemctl enable docker
               usermod -aG docker ubuntu
+
+              # Install AWS CLI  <-- ADD HERE
+              apt-get install -y unzip curl
+              curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+              unzip awscliv2.zip
+              sudo ./aws/install
 
               # Install Java
               apt-get install -y openjdk-17-jre-headless
@@ -103,6 +146,15 @@ resource "aws_eip" "devops_eip" {
 resource "aws_eip_association" "devops_eip_assoc" {
   instance_id   = aws_instance.devops_demo_ec2.id
   allocation_id = aws_eip.devops_eip.id
+}
+
+resource "aws_security_group_rule" "allow_ec2_to_rds" {
+  type                     = "ingress"
+  from_port                = 5432
+  to_port                  = 5432
+  protocol                 = "tcp"
+  security_group_id        = var.rds_security_group_id
+  source_security_group_id = aws_security_group.app_sg.id
 }
 
 output "app_ip" {
