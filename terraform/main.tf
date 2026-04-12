e25a0703220accb517959978217953be133176e6
p-kali <pravin.k.kalimuthu@gmail.com>
2026-04-12 18:44:35 -0400

    Added unregister role and fixed cicd to get instance id

--- terraform/main.tf
+++ terraform/main.tf
@@ -89,6 +89,26 @@ resource "aws_iam_instance_profile" "ec2_profile" {
   role = aws_iam_role.ec2_role.name
 }
 
+resource "aws_iam_role_policy" "alb_registration_policy" {
+  name = "alb-target-registration"
+  role = aws_iam_role.ec2_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "elasticloadbalancing:RegisterTargets",
+          "elasticloadbalancing:DeregisterTargets",
+          "elasticloadbalancing:DescribeTargetHealth"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
 resource "aws_instance" "devops_demo_ec2" {
   #ami = "ami-0ec10929233384c7f"
   #instance_type = "t3.micro"
